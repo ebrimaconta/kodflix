d384b8fdb87bdc408c881408d94a0d2317b3daf6
Ebrima <ebrimaconta@gmail.com>
2020-03-03 11:36:52 +0000

    Added overlay to images

--- src/App.js
+++ src/App.js
@@ -14,28 +14,41 @@ function App() {
        <div className="container">
         <div className="item">
           <img src={breakingBad} alt="Breaking bad" />
+          <div className="overlay">
+            <h1>Breaking Bad </h1>
+          </div>
         </div>
         <div className="item">
           <img src={batman} alt="Batman" />
-           
+          <div className="overlay">
+            <h1>Batman Begins</h1>
+          </div>
         </div>
         <div className="item">
           <img src={godfather} alt="Godfather" />
-           
+          <div className="overlay">
+            <h1>Godfather</h1>
+          </div>
         </div>
       </div>
       <div className="container">
         <div className="item">
           <img src={mywifeandkids} alt="My wife and kids" />
-           
+          <div className="overlay">
+            <h1>My wife and kids</h1>
+          </div>
         </div>
         <div className="item">
           <img src={onlyfoolsandhorses} alt="Only fools and horses" />
-           
+          <div className="overlay">
+            <h1>Only fools and horses</h1>
+          </div>
         </div>
         <div className="item">
           <img src={freshprince} alt="Fresh prince" />
-           
+          <div className="overlay">
+            <h1>Fresh prince</h1>
+          </div>
         </div>
       </div>
     </div>
