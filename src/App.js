ade623278f51e8c70c9333f5009cb2bd56c0e768
Ebrima <ebrimaconta@gmail.com>
2020-03-03 11:04:09 +0000

    Added 6 cover titles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import breakingBad from './breaking-bad.jpg';
+import batman from './batman.jpg';
+import freshprince  from './freshprince.jpg';
+import godfather from './godfather.jpg';
+import mywifeandkids from './mywifeandkids.jpg';
+import onlyfoolsandhorses from './onlyfoolsandhorses.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       Welcome to Kodflix 
-        <img src={breakingBad} alt="Breaking Bad"/>
+       <h1>Welcome to Kodflix </h1>
+       <div className="container">
+        <div className="item">
+          <img src={breakingBad} alt="Breaking bad" />
+        </div>
+        <div className="item">
+          <img src={batman} alt="Batman" />
+           
+        </div>
+        <div className="item">
+          <img src={godfather} alt="Godfather" />
+           
+        </div>
+      </div>
+      <div className="container">
+        <div className="item">
+          <img src={mywifeandkids} alt="My wife and kids" />
+           
+        </div>
+        <div className="item">
+          <img src={onlyfoolsandhorses} alt="Only fools and horses" />
+           
+        </div>
+        <div className="item">
+          <img src={freshprince} alt="Fresh prince" />
+           
+        </div>
+      </div>
     </div>
   );
 }
