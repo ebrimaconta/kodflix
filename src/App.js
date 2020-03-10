7d116dbc23084320700e27f6060a9cc3fd1408a8
Ebrima <ebrimaconta@gmail.com>
2020-03-10 09:42:21 +0000

    Create a Gallery component to show the covers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Gallery from './Gallery';
 import breakingBad from './breaking-bad.jpg';
 import batman from './batman.jpg';
 import freshprince  from './freshprince.jpg';
@@ -6,22 +7,13 @@ import godfather from './godfather.jpg';
 import mywifeandkids from './mywifeandkids.jpg';
 import onlyfoolsandhorses from './onlyfoolsandhorses.jpeg';
 import './App.css';
-import Movies from './Movies';
+
 
 function App() {
   return (
     <div className="App">
        <h1>Welcome to Kodflix </h1>
-       <div className="container">
-        <Movies name='Breaking bad' img={breakingBad} />
-        <Movies name='Batman' img={batman} />
-        <Movies name='Godfather' img={godfather} />
-      </div>
-      <div className="container">
-        <Movies name='My wife and kids' img={mywifeandkids} />
-        <Movies name='Only fools and horses' img={onlyfoolsandhorses} />
-        <Movies name='Fresh prince' img={freshprince} />
-      </div>
+       <Gallery /> 
     </div>
   );
 }
---
+++ src/Gallery.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Movies from './Movies';
+import breakingBad from './breaking-bad.jpg';
+import batman from './batman.jpg';
+import freshprince  from './freshprince.jpg';
+import godfather from './godfather.jpg';
+import mywifeandkids from './mywifeandkids.jpg';
+import onlyfoolsandhorses from './onlyfoolsandhorses.jpeg';
+
+function Gallery (props) {
+    return (
+    <div>
+     <div className="container">
+        <Movies name='Breaking bad' img={breakingBad} />
+        <Movies name='Batman' img={batman} />
+        <Movies name='Godfather' img={godfather} />
+      </div>
+      <div className="container">
+        <Movies name='My wife and kids' img={mywifeandkids} />
+        <Movies name='Only fools and horses' img={onlyfoolsandhorses} />
+        <Movies name='Fresh prince' img={freshprince} />
+      </div>
+      </div>
+    );
+  }
+
+export default Gallery; 
+ 
